c251c0e6e825070f133e5930f672865e0fea7f15
Arnab Basu <asrbaban@gmail.com>
2024-11-24 21:23:28 +0530

    express set up

--- server.js
+++ server.js
@@ -3,5 +3,10 @@ const dotenv=require('dotenv');
 
 dotenv.config({path:'./config/config.env'});
 const app=express();
-const port=process.env.PORT||5000;
-app.listen(port,console.log(`Server running on PORT-> ${port}`));
+app.get('/',(req,res)=>{
+    res.send('1st express-');
+});
+const port=process.env.PORT;
+
+
+app.listen(port,console.log(`Server running on PORT -> ${port}`));
